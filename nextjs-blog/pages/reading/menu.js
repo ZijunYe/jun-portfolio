6f2eb6e77c85e8ccabc3a7928d3b3e2bae8d03fe
Zijun <zijunye001@gmail.com>
2023-11-17 12:12:51 -0500

    New design for nav bar

---
+++ nextjs-blog/pages/reading/menu.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'; 
+import Layout from '../components/layout';
+
+export default function Menu(){
+    return(
+        <Layout>
+             <h1>📚Books</h1>
+            <h2>Coming soon!</h2>
+            <h3>
+                <Link href="/">Back to homepage</Link>
+            </h3>
+        </Layout>
+    ); 
+}
