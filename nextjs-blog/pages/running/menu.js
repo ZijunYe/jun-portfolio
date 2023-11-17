f2e193d38b5d6eb742fb5fdc1cdf5597b50e6091
Zijun <zijunye001@gmail.com>
2023-11-17 07:53:06 -0500

    Temporarily fix the nav bar expand issue + add responsive design

--- nextjs-blog/pages/running/menu.js
+++ nextjs-blog/pages/running/menu.js
@@ -1,10 +1,8 @@
 import Link from 'next/link'; 
 import Layout from '../components/layout';
 import styles from './runnerJournal.module.css';
-import React, { useEffect, useState } from 'react';
 
-
-export default function Journal(){
+export default function Menu(){
     const journals =[
         {
             count: '01.', 
