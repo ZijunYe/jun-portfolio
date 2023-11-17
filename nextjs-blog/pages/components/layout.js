c3cf5636ce3434510651d424bafb0b4959ca0050
Zijun <zijunye001@gmail.com>
2023-11-17 12:12:51 -0500

    New design for nav bar

--- nextjs-blog/pages/components/layout.js
+++ nextjs-blog/pages/components/layout.js
@@ -1,52 +1,50 @@
-import styles from './layout.module.css'; 
+import styles from './layout.module.css';
 import NavBar from './navBar';
-import { usePathname } from 'next/navigation'; 
-import { useState,useEffect } from 'react'; // Import useState hook
+import { usePathname } from 'next/navigation';
+import { useState, useEffect } from 'react'; // Import useState hook
 
-export default function Layout({children}){
-    const pathname = usePathname();
-    const name = (pathname.split('/'))[2]; 
-    console.log(name); 
-    // State to track whether the navbar should be visible
-    const [navbarVisible, setNavbarVisible] = useState(false);
+export default function Layout({ children }) {
+  const pathname = usePathname();
+  const name = (pathname.split('/'))[2];
+  console.log(name);
+  // State to track whether the navbar should be visible
+  const [navbarVisible, setNavbarVisible] = useState(false);
+  const [closing, setClosing] = useState(false); // New state to manage closing animation
 
-    useEffect(() => {
-        if (name === 'menu') {
-          setNavbarVisible(true);
-        }
-      }, [name, setNavbarVisible]);
-  
-    // Function to toggle navbar visibility
-    const toggleNavbar = () => {
-        setNavbarVisible(!navbarVisible);
-    };
+  useEffect(() => {
+    if (name === 'menu') {
+      setNavbarVisible(true);
+    }
+  }, [name, setNavbarVisible]);
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.navbar} style={{ display: navbarVisible ? 'block' : 'none' }}>
-                <NavBar></NavBar>
-            </div>
+  // Function to toggle navbar visibility and closing animation
+  const toggleNavbar = () => {
+    setClosing(!navbarVisible); // Set closing state before toggling visibility
+    setTimeout(() => {
+      setNavbarVisible(!navbarVisible);
+      setClosing(false);
+    }, 500); // Set the timeout duration to match the closeAnimation duration
+  };
 
-            <div className={styles.expand} style={{ display: navbarVisible ? 'block' : 'none' }}>
-                <div className={styles.expandIcon} onClick={toggleNavbar}>
-                    <img className={styles.iconV} src="/project-icons/Expand_left_double_light.png"></img>
-                    <img className={styles.iconH} src="/project-icons/Expand_up_double_light.png"></img>
-                    <div className={styles.horizontal}></div>
-                </div>
-                <div className={styles.vertical}></div>
-               
-            </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.navbar} style={{ display: navbarVisible ? 'flex' : 'none' }}>
+        <NavBar></NavBar>
 
-            <div className={styles.open} style={{ display: navbarVisible ? 'none' : 'block' }}>
-            <div onClick={toggleNavbar}>
-                    <img className={styles.icon} src="/project-icons/menu.png"></img>
-                </div>
+        <div className={styles.expand} onClick={toggleNavbar} style={{ display: navbarVisible ? 'block' : 'none' }}>
+          <img className={`${styles.iconV} ${closing ? styles.closing : ''}`} src="/project-icons/Expand_left_double_light.png"></img>
+          <img className={styles.iconH} src="/project-icons/Expand_up_double_light.png"></img>
+          {/* <div className={styles.horizontal}></div> */}
+        </div>
+     </div>
 
-            </div>
-            
-            <div className={styles.content}>{children}</div>
-           
+      <div className={styles.open} style={{ display: navbarVisible ? 'none' : 'block' }}>
+        <div onClick={toggleNavbar}>
+          <img className={styles.icon} src="/project-icons/menu.png"></img>
+        </div>
       </div>
-     
-    );
+
+      <div className={styles.content}>{children}</div>
+    </div>
+  );
 }
